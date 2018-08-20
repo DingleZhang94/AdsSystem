8591acf58050a859421877057774d02abb9b10d7
DingleZhang94 <dingle.zhang@gmail.com>
2018-08-20 19:31:05 -0400

    Initial commit

---
+++ src/db/mysql/MySQLAdsTableCreation.java
@@ -0,0 +1,60 @@
+package db.mysql;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class MySQLAdsTableCreation {
+	public static void main(String[] args) {
+		// connect to mysql
+		try {
+			Class.forName("com.mysql.jdbc.Driver").getConstructor().newInstance();
+			Connection conn = DriverManager.getConnection(MySQLDBUtil.URL);
+			
+			if (conn == null) {
+				return;
+			}
+			
+			Statement stmt = conn.createStatement();
+			
+			String sql = "DROP TABLE If EXISTS ad";
+			stmt.executeUpdate(sql);
+			
+			sql = "DROP TABLE IF EXISTS advertiser";
+			stmt.executeUpdate(sql);
+
+			// Step 3 Create new tables
+			
+			// create advertiser table
+			sql = "CREATE TABLE advertiser ("
+					+ "advertiser_id int NOT NULL AUTO_INCREMENT,"
+					+ "name VARCHAR(255),"
+					+ "budget FLOAT,"
+					+ "PRIMARY KEY (advertiser_id)"
+					+ ")";
+
+			System.out.println(sql);
+			stmt.executeUpdate(sql);
+			
+			// create ad table
+			sql = "CREATE TABLE ad ("
+					+ "ad_id int NOT NULL AUTO_INCREMENT,"
+					+ "bid FLOAT,"
+					+ "image_url VARCHAR(2083),"
+					+ "advertiser_id int NOT NULL,"
+					+ "ad_score float,"
+					+ "PRIMARY KEY (ad_id),"
+					+ "FOREIGN KEY (advertiser_id) REFERENCES advertiser(advertiser_id)"
+					+ ")";
+		
+			System.out.println(sql);
+			stmt.executeUpdate(sql);
+			System.out.println("Import done successfully");
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+}
